1c4f87b865e5d0b3d30ac06bd38077ce0146bed6
Rafael Broseghini <rafaellopesbroseghini@gmail.com>
2018-07-28 23:13:36 -0500

    persistent data

--- routes/index.js
+++ routes/index.js
@@ -25,24 +25,45 @@ router.post('/:pollId/vote', (req, res, next) => {
   console.log(choiceId);
   
   const choice = req.body.choice;
-  const identifier = `choices.${choiceId}.votes`;  
-  console.log(identifier)
-  Poll.findByIdAndUpdate({_id: req.params.pollId}, {$inc: {[identifier]: 1}}, {}, (err, numberAffected) => {
-    console.log(numberAffected)
-    
-    let Pusher = require('pusher');
-    let pusher = new Pusher({
-      appId: process.env.PUSHER_APP_ID,
-      key: process.env.PUSHER_APP_KEY,
-      secret: process.env.PUSHER_APP_SECRET,
-      cluster: process.env.PUSHER_APP_CLUSTER
+  const identifier = `choices[0].votes`;  
+  // console.log(identifier)
+
+
+
+  Poll.findById(
+    req.params.pollId,
+    function (err, _poll) {
+        console.log(_poll);
+        
+        /** Temporarily store labelOptions in a new variable because we cannot directly modify the document */
+        let _updateChoiceOptions = _poll.choices;
+        // console.log(_updateChoiceOptions);
+        
+        /** We need to iterate over the labelOptions array to check where Bob is */
+        _updateChoiceOptions.forEach(function (_label) {
+
+          if (_label._id == choiceId) {            
+            ++_label.votes;
+          } 
+
+        });
+
+        /** Update the documents labelOptions property with the temporary one we've created */
+        _poll.update({choices: _updateChoiceOptions}, function (err) {
+          let Pusher = require('pusher');
+          let pusher = new Pusher({
+            appId: process.env.PUSHER_APP_ID,
+            key: process.env.PUSHER_APP_KEY,
+            secret: process.env.PUSHER_APP_SECRET,
+            cluster: process.env.PUSHER_APP_CLUSTER
+          });
+      
+          let payload = { pollId: req.params.pollId, choice: choice };
+          pusher.trigger('poll-events', 'vote', payload, req.body.socketId);
+      
+          res.send('');
+        });
     });
-
-    let payload = { pollId: req.params.pollId, choice: choice };
-    pusher.trigger('poll-events', 'vote', payload, req.body.socketId);
-
-    res.send('');
-  });
 });
 
 module.exports = router;
