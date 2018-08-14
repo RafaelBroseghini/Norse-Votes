b384e895ba78f07df4b3e2884ae8bdbe93390c77
Rafael Broseghini <rafaellopesbroseghini@gmail.com>
2018-08-13 21:18:02 -0500

    jquery carousel and next prev arrows

--- public/javascripts/landing.js
+++ public/javascripts/landing.js
@@ -13,13 +13,20 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 });
 
-// Carousel index.ejs
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.carousel');
-  var instance = M.Carousel.init(elems, {
-    fullWidth: true
-    // indicators: true
+// Carousel jQuery.
+$(document).ready(function(){
+  $('.carousel').carousel({
+    fullWidth: true,
+    indicators: true
   });
+
+  $(document).keydown(function(event){
+    if(event.which == 39){
+      $('.carousel').carousel('next')
+    } else if (event.which == 37){
+      $('.carousel').carousel('prev')
+    }
+  })
 });
 
 //  Character counter new.ejs
